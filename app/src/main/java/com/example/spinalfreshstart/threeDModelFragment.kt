4361d932b46bc91b0e07b0e96e21d6ce6de9673f
aashkul <aashish.kulkarni33@gmail.com>
2023-09-14 11:36:08 +1200

    line 22 and 23 fixed in threeDModelFragment

--- app/src/main/java/com/example/spinalfreshstart/threeDModelFragment.kt
+++ app/src/main/java/com/example/spinalfreshstart/threeDModelFragment.kt
@@ -18,8 +18,8 @@ class threeDModelFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (arguments != null) {
-            mParam1 = arguments!!.getString(ARG_PARAM1)
-            mParam2 = arguments!!.getString(ARG_PARAM2)
+            mParam1 = requireArguments().getString(ARG_PARAM1)
+            mParam2 = requireArguments().getString(ARG_PARAM2)
         }
     }
 
