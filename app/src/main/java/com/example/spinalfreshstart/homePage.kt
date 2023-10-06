abb7e6810bc314b0c6779eacd0b1e2d60e35dcbd
aashkul <aashish.kulkarni33@gmail.com>
2023-10-07 09:47:15 +1300

    Graph plotting data dynamically

--- app/src/main/java/com/example/spinalfreshstart/homePage.kt
+++ app/src/main/java/com/example/spinalfreshstart/homePage.kt
@@ -20,14 +20,7 @@ import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.DataPointInterface
 import com.jjoe64.graphview.series.LineGraphSeries
 import android.icu.util.DateInterval
-
-
-
-/**
- * A simple [Fragment] subclass.
- * Use the [homePage.newInstance] factory method to
- * create an instance of this fragment.
- */
+import kotlin.system.exitProcess
 
 class homePage : Fragment() {
     //homeBranch created
@@ -44,7 +37,6 @@ class homePage : Fragment() {
             mParam2 = requireArguments().getString(ARG_PARAM2)
         }
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -78,214 +70,89 @@ class homePage : Fragment() {
             timerTwo.stop()
         }
 
+
         val modelActivity = ModelActivity()
-        val series2: LineGraphSeries<DataPoint> = LineGraphSeries()
+        val series1: LineGraphSeries<DataPoint> = LineGraphSeries()
         var dataIndex = 0
+        val handler = Handler(Looper.getMainLooper())
+        var time = 0.0
+        var maxLimit = modelActivity.sampleAngles.size - 1
 
+        fun harmfulAngleFun() {
+            val max = series1.highestValueY;
+            val min = series1.lowestValueY;
+            var harmfulAngle = (max-min)*(0.8)+min;
+            var harmfulAngleMyRef: DatabaseReference = modelActivity.database.getReference("harmfulAngle")
+            harmfulAngleMyRef.setValue(harmfulAngle)
 
+            val values = series1.getValues(min,max)
+            var count1 = 0.0
+            var totalCount= 0.0
+            for (dataPoint in  values) {
+                val yValue = dataPoint.y // Get the y-value of the data point
+                if(yValue>harmfulAngle){
+                    println("y-Value is $yValue")
+                    count1 += 1.0
+                    totalCount++
+                } else {
+                    totalCount++
+                    continue
+                }
+            }
 
-//        val timeInter
-//
-//
-//        val newRun = object : Runnable {
-//            override fun run()
-//                val time = timeIntervals[dataIndex]
-//
-//
-//            }
-//
-//        }
-
-        val series1: LineGraphSeries<DataPoint> = LineGraphSeries(
-            arrayOf(
-                DataPoint(0.0, 10.3),
-                DataPoint(0.2, 9.7),
-                DataPoint(0.4, 9.0),
-                DataPoint(0.6, 8.3),
-                DataPoint(0.8, 7.6),
-                DataPoint(1.0, 7.0),
-                DataPoint(1.2, 6.4),
-                DataPoint(1.4, 5.9),
-                DataPoint(1.6, 5.6),
-                DataPoint(1.8, 5.1),
-                DataPoint(2.0, 4.4),
-                DataPoint(2.2, 3.6),
-                DataPoint(2.4, 2.9),
-                DataPoint(2.6, 2.4),
-                DataPoint(2.8, 2.2),
-                DataPoint(3.0, 2.1),
-                DataPoint(3.2, 2.0),
-                DataPoint(3.4, 1.9),
-                DataPoint(3.6, 2.2),
-                DataPoint(3.8, 3.0),
-                DataPoint(4.0, 4.4),
-                DataPoint(4.2, 5.8),
-                DataPoint(4.4, 7.1),
-                DataPoint(4.6, 8.3),
-                DataPoint(4.8, 9.4),
-                DataPoint(5.0, 10.3),
-                DataPoint(5.2, 0.0),
-                DataPoint(5.4, 15.3),
-                DataPoint(5.6, 16.3),
-                DataPoint(5.8, 17.0),
-                DataPoint(6.0, 17.5),
-                DataPoint(6.2, 18.5),
-                DataPoint(6.4, 19.1),
-                DataPoint(6.6, 20.3),
-                DataPoint(6.8, 21.4),
-                DataPoint(7.0, 22.4),
-                DataPoint(7.2, 23.6),
-                DataPoint(7.4, 24.7),
-                DataPoint(7.6, 26.1),
-                DataPoint(7.8, 27.4),
-                DataPoint(8.0, 28.8),
-                DataPoint(8.2, 30.1),
-                DataPoint(8.4, 31.5),
-                DataPoint(8.6, 33.1),
-                DataPoint(8.8, 34.5),
-                DataPoint(9.0, 35.7),
-                DataPoint(9.2, 37.1),
-                DataPoint(9.4, 38.4),
-                DataPoint(9.6, 39.8),
-                DataPoint(9.8, 41.2),
-                DataPoint(10.0, 42.8),
-                DataPoint(10.2, 44.4),
-                DataPoint(10.4, 45.9),
-                DataPoint(10.6, 47.4),
-                DataPoint(10.8, 48.8),
-                DataPoint(11.0, 50.3),
-                DataPoint(11.2, 51.9),
-                DataPoint(11.4, 53.4),
-                DataPoint(11.6, 54.6),
-                DataPoint(11.8, 55.8),
-                DataPoint(12.0, 56.8),
-                DataPoint(12.2, 57.8),
-                DataPoint(12.4, 58.7),
-                DataPoint(12.6, 59.5),
-                DataPoint(12.8, 60.2),
-                DataPoint(13.0, 60.8),
-                DataPoint(13.2, 61.2),
-                DataPoint(13.4, 61.7),
-                DataPoint(13.6, 62.0),
-                DataPoint(13.8, 62.2),
-                DataPoint(14.0, 62.5),
-                DataPoint(14.2, 62.9),
-                DataPoint(14.4, 63.0),
-                DataPoint(14.6, 62.8),
-                DataPoint(14.8, 62.6),
-                DataPoint(15.0, 62.3),
-                DataPoint(15.2, 61.9),
-                DataPoint(15.4, 61.3),
-                DataPoint(15.6, 60.4),
-                DataPoint(15.8, 59.1),
-                DataPoint(16.0, 57.6),
-                DataPoint(16.2, 55.5),
-                DataPoint(16.4, 53.5),
-                DataPoint(16.6, 51.4),
-                DataPoint(16.8, 49.0),
-                DataPoint(17.0, 46.6),
-                DataPoint(17.2, 44.0),
-                DataPoint(17.4, 41.4),
-                DataPoint(17.6, 38.8),
-                DataPoint(17.8, 36.3),
-                DataPoint(18.0, 34.0),
-                DataPoint(18.2, 32.0),
-                DataPoint(18.4, 29.9),
-                DataPoint(18.6, 27.9),
-                DataPoint(18.8, 26.0),
-                DataPoint(19.0, 24.1),
-                DataPoint(19.2, 22.1),
-                DataPoint(19.4, 19.9),
-                DataPoint(19.6, 17.7),
-                DataPoint(19.8, 15.5),
-                DataPoint(20.0, 13.3),
-                DataPoint(20.2, 10.9),
-                DataPoint(20.4, 8.7),
-                DataPoint(20.6, 6.4),
-                DataPoint(20.8, 4.4),
-                DataPoint(21.0, 2.5),
-                DataPoint(21.2, 1.3),
-                DataPoint(21.4, 2.0),
-                DataPoint(21.6, 3.8),
-                DataPoint(21.8, 5.5),
-                DataPoint(22.0, 7.2),
-                DataPoint(22.2, 8.8),
-                DataPoint(22.4, 10.5),
-                DataPoint(22.6, 12.1),
-                DataPoint(22.8, 13.8),
-                DataPoint(23.0, 15.2),
-                DataPoint(23.2, 16.4),
-                DataPoint(23.4, 17.3),
-                DataPoint(23.6, 18.0),
-                DataPoint(23.8, 18.4),
-                DataPoint(24.0, 18.3),
-                DataPoint(24.2, 18.4),
-                DataPoint(24.4, 18.4),
-                DataPoint(24.6, 18.5),
-                DataPoint(24.8, 18.5),
-                DataPoint(25.0, 18.5)
-            )
-        )
-
-        val max = series1.highestValueY;
-        val min = series1.lowestValueY;
-        val harmfulAngle = (max-min)*(0.8)+min;
-
-
-        val harmfulAngleMyRef: DatabaseReference = modelActivity.database.getReference("harmfulAngle")
-        harmfulAngleMyRef.setValue(harmfulAngle)
-
-        fun drawStraightLineFromYAxisValue(graphView: GraphView, angle: Float) {
-            val lineGraphSeries = LineGraphSeries<DataPoint>()
-            val harmfulAngleDouble = angle.toDouble()
-            val startPoint = DataPoint(series1.lowestValueX,  harmfulAngleDouble)
-            val endPoint = DataPoint(series1.highestValueX,  harmfulAngleDouble)
-            lineGraphSeries.color = Color.RED
-            lineGraphSeries.appendData(startPoint, true, 2)
-            lineGraphSeries.appendData(endPoint, true, 2)
-            graphView.addSeries(lineGraphSeries)
+            var ratio = count1/totalCount
+            var harmfulAngleDuration = series1.highestValueX * ratio
+            println("Harmful angle: ${harmfulAngle.toFloat()}")
+            println("harmfulAngleCount is $count1")
+            println("totalCount is $totalCount")
+            println("ratio is $ratio")
+            println("Harmful angle duration: "+ harmfulAngleDuration +" out of "+ series1.highestValueX)
+            val floatHarmfulAngleDuration = String.format("%.2f", harmfulAngleDuration.toFloat()).toFloat()
+            val angleTextView = view?.findViewById<TextView>(R.id.textHarmfulAngle)
+            angleTextView?.text= floatHarmfulAngleDuration.toString()
         }
 
-        drawStraightLineFromYAxisValue(lineGraphView, harmfulAngle.toFloat())
+        val newRun = object : Runnable {
+            override fun run(){
+                if(dataIndex != modelActivity.sampleAngles.size) {
+                    val angleData = modelActivity.sampleAngles[dataIndex]
+                    val dataPoint = DataPoint(time, angleData.toDouble())
+                    series1.appendData(dataPoint, true, maxLimit)
+                    println("working!")
+                    lineGraphView.addSeries(series1)
 
-        val values = series1.getValues(min,max)
-        var count1 = 0.0
-        var totalCount= 0.0
-        for (dataPoint in  values) {
-            val yValue = dataPoint.y // Get the y-value of the data point
-            if(yValue>harmfulAngle){
-                println("y-Value is $yValue")
-                count1 += 1.0
-                totalCount++
-            } else {
-                totalCount++
-                continue
+                    harmfulAngleFun()
+                    dataIndex += 1
+                    time += 0.2
+                    handler.postDelayed(this, 50)
+                } else {
+
+                    val maximum = series1.highestValueY;
+                    val minimum = series1.lowestValueY;
+                    val harmfulAngle = (maximum-minimum)*(0.8)+minimum;
+
+                    fun drawStraightLineFromYAxisValue(graphView: GraphView, angle: Float) {
+                        val lineGraphSeries = LineGraphSeries<DataPoint>()
+                        val harmfulAngleDouble = angle.toDouble()
+                        val startPoint = DataPoint(series1.lowestValueX,  harmfulAngleDouble)
+                        val endPoint = DataPoint(series1.highestValueX,  harmfulAngleDouble)
+                        lineGraphSeries.color = Color.RED
+                        lineGraphSeries.appendData(startPoint, true, 2)
+                        lineGraphSeries.appendData(endPoint, true, 2)
+                        graphView.addSeries(lineGraphSeries)
+                    }
+
+                    drawStraightLineFromYAxisValue(lineGraphView, harmfulAngle.toFloat())
+
+                }
             }
         }
-        var ratio = count1/totalCount
-        var harmfulAngleDuration = series1.highestValueX * ratio
 
-        println("harmfulAngleCount is $count1")
-        println("totalCount is $totalCount")
-        println("ratio is $ratio")
-        println("Harmful angle duration: "+ harmfulAngleDuration +" out of "+ series1.highestValueX)
-
-        val floatHarmfulAngleDuration = String.format("%.2f", harmfulAngleDuration.toFloat()).toFloat()
-
-
-        val angleTextView = view?.findViewById<TextView>(R.id.textHarmfulAngle)
-        angleTextView?.text= floatHarmfulAngleDuration.toString()
-
-
+        handler.post(newRun)
         lineGraphView.animate()
-        lineGraphView.viewport.isScrollable = true
-        lineGraphView.viewport.isScalable = true
-        lineGraphView.viewport.setScalableY(true)
-        lineGraphView.viewport.setScrollableY(true)
         series1.color = R.color.purple_200
         lineGraphView.addSeries(series1)
 
-        // Inflate the layout for this fragment
         return view
     }
 
