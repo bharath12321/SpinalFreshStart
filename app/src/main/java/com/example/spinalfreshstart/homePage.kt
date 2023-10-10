ad4b2c8295bee7cdb4eb11c076afb68e70538e96
aashkul <aashish.kulkarni33@gmail.com>
2023-10-10 15:31:34 +1300

    Home page updated, home page timers removed

--- app/src/main/java/com/example/spinalfreshstart/homePage.kt
+++ app/src/main/java/com/example/spinalfreshstart/homePage.kt
@@ -1,5 +1,6 @@
 package com.example.spinalfreshstart
 import akka.http.scaladsl.server.RejectionHandler
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
@@ -46,30 +47,29 @@ class homePage : Fragment() {
 
         lineGraphView = view.findViewById(R.id.graphModel)
 
-        val timerOne = view.findViewById<Chronometer>(R.id.chronometerOne)
-        val timerTwo = view.findViewById<Chronometer>(R.id.chronometerTwo)
+      //  val timerOne = view.findViewById<Chronometer>(R.id.chronometerOne)
+      //  val timerTwo = view.findViewById<Chronometer>(R.id.chronometerTwo)
 
-        val startTimerOne = view.findViewById<Button>(R.id.startbuttonTimerOne)
-        val stopTimerOne = view.findViewById<Button>(R.id.stopbuttonTimerOne)
-        val startTimerTwo = view.findViewById<Button>(R.id.startbuttonTimerTwo)
-        val stopTimerTwo = view.findViewById<Button>(R.id.stopbuttonTimerTwo)
-
-
-        startTimerOne.setOnClickListener {
-            timerOne.base = SystemClock.elapsedRealtime()
-            timerOne.start()
-        }
-        stopTimerOne.setOnClickListener {
-            timerOne.stop()
-        }
-        startTimerTwo.setOnClickListener {
-            timerTwo.base = SystemClock.elapsedRealtime()
-            timerTwo.start()
-        }
-        stopTimerTwo.setOnClickListener {
-            timerTwo.stop()
-        }
-
+//        val startTimerOne = view.findViewById<Button>(R.id.startbuttonTimerOne)
+//        val stopTimerOne = view.findViewById<Button>(R.id.stopbuttonTimerOne)
+//        val startTimerTwo = view.findViewById<Button>(R.id.startbuttonTimerTwo)
+    //    val timer = view.findViewById<Button>(R.id.chronometerTwo)
+//
+//
+//        startTimerOne.setOnClickListener {
+//            timerOne.base = SystemClock.elapsedRealtime()
+//            timerOne.start()
+//        }
+//        stopTimerOne.setOnClickListener {
+//            timerOne.stop()
+//        }
+//        startTimerTwo.setOnClickListener {
+//            timerTwo.base = SystemClock.elapsedRealtime()
+//            timerTwo.start()
+//        }
+//        stopTimerTwo.setOnClickListener {
+//            timerTwo.stop()
+//        }
 
         val modelActivity = ModelActivity()
         val series1: LineGraphSeries<DataPoint> = LineGraphSeries()
@@ -84,6 +84,24 @@ class homePage : Fragment() {
             var harmfulAngle = (max-min)*(0.8)+min;
             var harmfulAngleMyRef: DatabaseReference = modelActivity.database.getReference("harmfulAngle")
             harmfulAngleMyRef.setValue(harmfulAngle)
+
+            //harmful angle
+            val harmfulAngleTextView1 = view?.findViewById<TextView>(R.id.harmfulAngleNew)
+            val floatHarmfulAngle = String.format("%.2f", harmfulAngle.toFloat()).toFloat()
+            harmfulAngleTextView1?.text= floatHarmfulAngle.toString()
+
+            //max angle
+            val maxAngleTextView = view?.findViewById<TextView>(R.id.maxAngleValue)
+            val floatMaxAngle = String.format("%.2f", max.toFloat()).toFloat()
+            maxAngleTextView?.text= floatMaxAngle.toString()
+
+            //min angle
+            val minAngleTextView = view?.findViewById<TextView>(R.id.minAngleValue)
+            val floatMinAngle = String.format("%.2f", min.toFloat()).toFloat()
+            minAngleTextView?.text= floatMinAngle.toString()
+
+
+
 
             val values = series1.getValues(min,max)
             var count1 = 0.0
